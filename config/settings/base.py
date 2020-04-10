01052c11f2dc318df3737852cebd542bb4d5ef77
jorgesaw <jorgesaw@gmail.com>
2020-04-10 03:07:27 -0300

    Core app created

--- config/settings/base.py
+++ config/settings/base.py
@@ -59,7 +59,7 @@ THIRD_PARTY_APPS = [
 ]
 
 LOCAL_APPS = [
- 
+    'apps.core.apps.CoreAppConfig', 
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,7 @@
 """Main URLs module."""
 
 from django.conf import settings
-from django.urls import path
+from django.urls import include, path
 from django.conf.urls.static import static
 from django.contrib import admin
 
@@ -9,6 +9,9 @@ from django.contrib import admin
 urlpatterns = [
     # Django Admin
     path(settings.ADMIN_URL, admin.site.urls),
+    
+    # Core app
+    path('', include(('apps.core.urls', 'core'), namespace='core')),
 ]
 
 if settings.DEBUG:
